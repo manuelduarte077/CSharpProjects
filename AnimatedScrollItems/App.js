419b6c184d08feb2f55edb3b6c87a892ed3d14b5
Manuel Duarte  <carlosd99m@gmail.com>
2023-05-10 22:38:14 -0600

    scroll Item animation effect

--- AnimatedScrollItems/App.js
+++ AnimatedScrollItems/App.js
@@ -30,32 +30,101 @@ const DATA = [...Array(30).keys()].map((_, i) => {
 
 const SPACING = 20;
 const AVATAR_SIZE = 70;
+const ITEM_SIZE = AVATAR_SIZE + SPACING * 3;
+
+/// IMG
+const BG_IMG =
+  "https://images.pexels.com/photos/1231265/pexels-photo-1231265.jpeg";
 
 export default () => {
+  const scrollY = React.useRef(new Animated.Value(0)).current;
+
   return (
     <View style={{ flex: 1, backgroundColor: "#fff" }}>
       <StatusBar hidden />
 
-      <FlatList
+      <Image
+        source={{ uri: BG_IMG }}
+        style={StyleSheet.absoluteFillObject}
+        blurRadius={80}
+      />
+
+      <Animated.FlatList
         data={DATA}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+          { useNativeDriver: true }
+        )}
         keyExtractor={(item) => item.key}
+        contentContainerStyle={{
+          padding: SPACING,
+          paddingTop: StatusBar.currentHeight || 72,
+        }}
         renderItem={({ item, index }) => {
+          const inputRange = [
+            -1,
+            0,
+            ITEM_SIZE * index,
+            ITEM_SIZE * (index + 2),
+          ];
+
+          const opacityInputRange = [
+            -1,
+            0,
+            ITEM_SIZE * index,
+            ITEM_SIZE * (index + 0.5),
+          ];
+
+          const scale = scrollY.interpolate({
+            inputRange,
+            outputRange: [1, 1, 1, 0],
+          });
+
+          const opacity = scrollY.interpolate({
+            inputRange: opacityInputRange,
+            outputRange: [1, 1, 1, 0],
+          });
+
           return (
-            <View style={{ flexDirection: "row" }}>
+            <Animated.View
+              style={{
+                flexDirection: "row",
+                padding: SPACING,
+                marginBottom: SPACING,
+                borderRadius: 12,
+                backgroundColor: "rgba(255,255,255,0.8)",
+                shadowColor: "#000",
+                shadowOffset: {
+                  width: 0,
+                  height: 10,
+                },
+                shadowOpacity: 0.3,
+                shadowRadius: 20,
+                opacity,
+                transform: [{ scale }],
+              }}
+            >
               <Image
                 source={{ uri: item.image }}
                 style={{
                   width: AVATAR_SIZE,
                   height: AVATAR_SIZE,
                   borderRadius: AVATAR_SIZE,
+                  marginRight: SPACING / 2,
                 }}
               />
               <View>
-                <Text>{item.name}</Text>
-                <Text>{item.jobTitle}</Text>
-                <Text>{item.email}</Text>
+                <Text style={{ fontSize: 22, fontWeight: "700" }}>
+                  {item.name}
+                </Text>
+                <Text style={{ fontSize: 18, opacity: 0.7 }}>
+                  {item.jobTitle}
+                </Text>
+                <Text style={{ fontSize: 14, opacity: 0.8, color: "#0099cc" }}>
+                  {item.email}
+                </Text>
               </View>
-            </View>
+            </Animated.View>
           );
         }}
       />
