f09e920a776b2c4cb143ac709ec1ee9ecdc51d37
Manuel Duarte  <carlosd99m@gmail.com>
2023-05-10 20:37:14 -0600

    refactor code

--- AnimatedScrollItems/App.js
+++ AnimatedScrollItems/App.js
@@ -1,31 +1,64 @@
-import * as React from 'react';
-import { StatusBar, FlatList, Image, Animated, Text, View, Dimensions, StyleSheet, TouchableOpacity, Easing, SafeAreaViewBase, SafeAreaView } from 'react-native';
-const { width, height } = Dimensions.get('screen');
-import faker from 'faker'
+import * as React from "react";
+import {
+  StatusBar,
+  FlatList,
+  Image,
+  Animated,
+  Text,
+  View,
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
+  Easing,
+  SafeAreaViewBase,
+  SafeAreaView,
+} from "react-native";
+const { width, height } = Dimensions.get("screen");
+import { faker } from "@faker-js/faker";
 
 faker.seed(10);
 const DATA = [...Array(30).keys()].map((_, i) => {
-    return {
-        key: faker.random.uuid(),
-        image: `https://randomuser.me/api/portraits/${faker.helpers.randomize(['women', 'men'])}/${faker.random.number(60)}.jpg`,
-        name: faker.name.findName(),
-        jobTitle: faker.name.jobTitle(),
-        email: faker.internet.email(),
-    };
+  return {
+    key: faker.datatype.uuid(),
+    // image: `https://randomuser.me/api/portraits/${faker.random.numeric(60)}.jpg`,
+    image: faker.image.avatar(),
+    name: faker.name.firstName(),
+    jobTitle: faker.name.jobTitle(),
+    email: faker.internet.email(),
+  };
 });
 
 const SPACING = 20;
 const AVATAR_SIZE = 70;
 
 export default () => {
-    return <View style={{flex: 1, backgroundColor: '#fff'}}>
-        <StatusBar hidden/>
-    
-        <FlatList 
+  return (
+    <View style={{ flex: 1, backgroundColor: "#fff" }}>
+      <StatusBar hidden />
 
-            data={DATA}
-
-        />
-
+      <FlatList
+        data={DATA}
+        keyExtractor={(item) => item.key}
+        renderItem={({ item, index }) => {
+          return (
+            <View style={{ flexDirection: "row" }}>
+              <Image
+                source={{ uri: item.image }}
+                style={{
+                  width: AVATAR_SIZE,
+                  height: AVATAR_SIZE,
+                  borderRadius: AVATAR_SIZE,
+                }}
+              />
+              <View>
+                <Text>{item.name}</Text>
+                <Text>{item.jobTitle}</Text>
+                <Text>{item.email}</Text>
+              </View>
+            </View>
+          );
+        }}
+      />
     </View>
-}
+  );
+};
