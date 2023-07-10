3daff5a3df3f0b4c109c07e3b30ed91b7110ea6c
Manuel Duarte  <carlosd99m@gmail.com>
2023-07-09 20:14:23 -0600

    product list card

--- ecommerce/App.js
+++ ecommerce/App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Switch, Text, View, SafeAreaView } from 'react-native'
+
+import { useColorScheme } from 'nativewind'
+import ProductsList from './components/ProductsList'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const { colorScheme, toggleColorScheme } = useColorScheme()
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <SafeAreaView className="flex-1 items-center justify-center bg-gray-200 dark:bg-black">
+      <View className="flex-row w-full gap-5">
+        <Text className="text-2xl font-bold dark:text-white">
+          New Collection
+        </Text>
+        <Switch value={colorScheme == 'dark'} onChange={toggleColorScheme} />
+      </View>
+      <ProductsList />
+
+      <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
+    </SafeAreaView>
+  )
+}
