7882af36bee9cf74fae9d8dd77e79c2f1f229e1c
Manuel Duarte  <carlosd99m@gmail.com>
2023-08-19 18:40:04 -0600

    scroll infinito

--- react-native-pokedex/App.tsx
+++ react-native-pokedex/App.tsx
@@ -1,23 +1,60 @@
 import { StatusBar } from 'expo-status-bar'
 import { useState, useEffect } from 'react'
-import { StyleSheet, Text, View, FlatList } from 'react-native'
+import {
+  StyleSheet,
+  Text,
+  FlatList,
+  ActivityIndicator,
+  SafeAreaView,
+} from 'react-native'
+
+interface Pokemon {
+  name: string
+  url: string
+}
 
 export default function App() {
   const [pokemon, setPokemon] = useState([])
+  const [next, setNext] = useState('')
+
+  const [isLoadingMore, setLoadingMore] = useState(false)
 
   useEffect(() => {
     fetch('https://pokeapi.co/api/v2/pokemon/')
       .then((response) => response.json())
       .then((data) => {
         setPokemon(data.results)
+        setNext(data.next)
       })
   }, [])
 
+  const loadMore = () => {
+    if (isLoadingMore) return
+    if (next) {
+      setLoadingMore(true)
+      fetch(next)
+        .then((response) => response.json())
+        .then((data) => {
+          setPokemon((prevPokemon) => [...prevPokemon, ...data.results])
+          setNext(data.next)
+          setLoadingMore(false)
+        })
+    }
+  }
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-      <Text>PokedexAPP</Text>
-    </View>
+      <FlatList
+        data={pokemon}
+        keyExtractor={(item) => item.name}
+        renderItem={({ item }) => <Text>{item.name}</Text>}
+        onEndReached={loadMore}
+        ListFooterComponent={() =>
+          isLoadingMore ? <ActivityIndicator /> : null
+        }
+      />
+    </SafeAreaView>
   )
 }
 
