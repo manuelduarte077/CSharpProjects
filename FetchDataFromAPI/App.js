5b79d086bbcace66c1fbb1bc3f68acd9cbe8c2f8
Manuel Duarte  <carlosd99m@gmail.com>
2023-12-12 22:20:13 -0600

    Add new files and configurations for React Native project

--- FetchDataFromAPI/App.js
+++ FetchDataFromAPI/App.js
@@ -2,18 +2,18 @@ import {SafeAreaView, StyleSheet} from 'react-native'
 import ProductListScreen from './screens/ProductListScreen'
 
 export default function App() {
-    return (
-        <SafeAreaView style={styles.container}>
-            <ProductListScreen/>
-        </SafeAreaView>
-    )
+  return (
+    <SafeAreaView style={styles.container}>
+      <ProductListScreen/>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 })
---
+++ React-Native/01-react-foundation/src/typescript/BasicTypes.tsx
@@ -0,0 +1,7 @@
+export const BasicTypes = () => {
+  return (
+    <>
+      <h3>Tipos Basicos</h3>
+    </>
+  )
+}
