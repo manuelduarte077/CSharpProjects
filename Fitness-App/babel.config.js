a98bcbffd8fc007cb5e62b71e234451073b9ddf2
Manuel Duarte  <carlosd99m@gmail.com>
2023-12-30 23:24:22 -0600

    Add new files and update configurations

--- Fitness-App/babel.config.js
+++ Fitness-App/babel.config.js
@@ -1,12 +1,12 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     plugins: [
       // Required for expo-router
-      'expo-router/babel',
+      "expo-router/babel",
       "nativewind/babel",
-      'react-native-reanimated/plugin'
+      "react-native-reanimated/plugin",
     ],
   };
 };
