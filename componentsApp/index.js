e6045be279f84caa59c8b09db46fbce3a8a264f5
Manuel Duarte <carlosd99m@gmail.com>
2024-09-04 23:32:41 -0600

    react basic

--- componentsApp/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import {name as appName} from './app.json';
-import {ComponentsApp} from './src/ComponentsApp';
-
-AppRegistry.registerComponent(appName, () => ComponentsApp);
