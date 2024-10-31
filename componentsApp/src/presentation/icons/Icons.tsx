39f2fe358335e7fda6a859cb067110b84019de8c
Manuel Duarte  <carlosd99m@gmail.com>
2024-10-31 00:14:53 -0600

    react basic (#5)

--- componentsApp/src/presentation/icons/Icons.tsx
+++
@@ -1,4 +0,0 @@
-import Icon from 'react-native-vector-icons/Ionicons';
-import React from 'react';
-
-export const AirplaneIcon = () => <Icon name="airplane-outline" size={30} />;
---
+++ react/counter-app/src/base-pruebas/02-template-string.js
@@ -0,0 +1,13 @@
+const nombre = "Manuel";
+const apellido = "Duarte";
+
+// const nombreCompleto = nombre + ' ' + apellido;
+const nombreCompleto = `${nombre} ${apellido}`;
+
+console.log(nombreCompleto);
+
+function getSaludo(nombre) {
+  return "Hola " + nombre;
+}
+
+console.log(`Este es un texto: ${getSaludo(nombre)}  `);
